469a8bc3683020f2f1a8f54672dd9b29d88c29de
DrHenryBruhns <71009746+DrHenryBruhns@users.noreply.github.com>
2020-11-20 11:28:26 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,22 +1,33 @@
 radio.onReceivedString(function (receivedString) {
     radiostring = receivedString
+    outputradio = ""
     if (radiostring.substr(0, 4) == "COM?") {
-        radio.sendString("" + input.compassHeading() + String.fromCharCode(linefeed))
+        outputradio = "COM=" + input.compassHeading() + String.fromCharCode(linefeed)
+        sema = true
     } else if (radiostring.substr(0, 4) == "ACC?") {
-        radio.sendString("" + input.acceleration(Dimension.Strength) + String.fromCharCode(linefeed))
+        outputradio = "ACC=" + input.acceleration(Dimension.Strength) + String.fromCharCode(linefeed)
+        sema = true
     } else if (radiostring.substr(0, 4) == "TMP?") {
         radio.sendString("" + input.temperature() + String.fromCharCode(linefeed))
     } else if (radiostring.substr(0, 4) == "ACX?") {
         radio.sendString("" + input.acceleration(Dimension.X) + String.fromCharCode(linefeed))
     } else {
-        radio.sendString("" + radiostring + " ?" + String.fromCharCode(linefeed))
+        radio.sendString("" + radiostring + "????" + String.fromCharCode(linefeed))
     }
-    radiostring = ""
 })
+let outputradio = ""
 let radiostring = ""
+let sema = false
 let linefeed = 0
 radio.setGroup(1)
 radio.setTransmitPower(7)
 radio.setFrequencyBand(0)
 linefeed = 10
+sema = false
 basic.showIcon(IconNames.Yes)
+basic.forever(function () {
+    if (sema == true) {
+        radio.sendString(outputradio)
+        sema = false
+    }
+})
