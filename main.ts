87e336c0b7cff1ed37baa3cb6c36d081b3a2f662
DrHenryBruhns <71009746+DrHenryBruhns@users.noreply.github.com>
2020-11-24 15:42:06 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -51,6 +51,7 @@ input.onButtonPressed(Button.B, function () {
 })
 let start = 0
 let UNNE = false
+let UNTI = false
 let TILT = false
 let cos = 0
 let clim = 0
@@ -78,154 +79,150 @@ refz = input.acceleration(Dimension.Z)
 refa = input.acceleration(Dimension.Strength)
 refcos = (input.acceleration(Dimension.X) * refx + input.acceleration(Dimension.Y) * refy + input.acceleration(Dimension.Z) * refz) / (input.acceleration(Dimension.Strength) * refa)
 let outputradio = "COM=" + input.compassHeading() + linefeed
-noa = false
-nob = false
 basic.forever(function () {
-    if (noa == true) {
-        if (nob == true) {
-            if (true) {
-                if (radiostring.substr(0, 4) == "COM?") {
-                    outputradio = "COM=" + input.compassHeading() + linefeed
-                    sema = true
-                } else if (radiostring.substr(0, 4) == "ACC?") {
-                    outputradio = "ACC=" + input.acceleration(Dimension.Strength) + linefeed
-                    sema = true
-                } else if (radiostring.substr(0, 4) == "TMP?") {
-                    outputradio = "TMP=" + input.temperature() + linefeed
-                    sema = true
-                } else if (radiostring.substr(0, 4) == "ACX?") {
-                    outputradio = "ACX=" + input.acceleration(Dimension.X) + linefeed
-                    sema = true
-                } else if (radiostring.substr(0, 4) == "REF!") {
-                    refx = input.acceleration(Dimension.X)
-                    refy = input.acceleration(Dimension.Y)
-                    refz = input.acceleration(Dimension.Z)
-                    refa = input.acceleration(Dimension.Strength)
-                    outputradio = "DONE" + linefeed
-                } else if (radiostring.substr(0, 4) == "COS?") {
-                    outputradio = "" + cos + linefeed
-                    sema = true
-                } else {
-                    outputradio = "????" + radioin + linefeed
-                    sema = true
-                }
-                radio.sendString(outputradio)
-                sema = false
-                radiostring = ""
+    if (radiostring.substr(0, 4) == "COM?") {
+        outputradio = "COM=" + input.compassHeading() + linefeed
+        sema = true
+    } else if (radiostring.substr(0, 4) == "ACC?") {
+        outputradio = "ACC=" + input.acceleration(Dimension.Strength) + linefeed
+        sema = true
+    } else if (radiostring.substr(0, 4) == "TMP?") {
+        outputradio = "TMP=" + input.temperature() + linefeed
+        sema = true
+    } else if (radiostring.substr(0, 4) == "ACX?") {
+        outputradio = "ACX=" + input.acceleration(Dimension.X) + linefeed
+        sema = true
+    } else if (radiostring.substr(0, 4) == "REF!") {
+        refx = input.acceleration(Dimension.X)
+        refy = input.acceleration(Dimension.Y)
+        refz = input.acceleration(Dimension.Z)
+        refa = input.acceleration(Dimension.Strength)
+        outputradio = "DONE" + linefeed
+        sema = true
+    } else if (radiostring.substr(0, 4) == "COS?") {
+        outputradio = "" + cos + linefeed
+        sema = true
+    } else if (radiostring.substr(0, 4) == "MAX!") {
+        max = (input.acceleration(Dimension.X) * refx + input.acceleration(Dimension.Y) * refy + input.acceleration(Dimension.Z) * refz) / (input.acceleration(Dimension.Strength) * refa)
+        clim = refcos - max
+        outputradio = "DONE" + linefeed
+        sema = true
+    } else {
+        outputradio = "????" + radioin + linefeed
+        sema = true
+    }
+    radio.sendString(outputradio)
+    sema = false
+    radiostring = ""
+    cos = (input.acceleration(Dimension.X) * refx + input.acceleration(Dimension.Y) * refy + input.acceleration(Dimension.Z) * refz) / (input.acceleration(Dimension.Strength) * refa)
+    if (cos < refcos - 0.8 * clim) {
+        outputradio = "COSE" + linefeed
+        radio.sendString(outputradio)
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+        TILT = true
+    } else if (cos > refcos - 0.8 * clim && cos < refcos - 0.6 * clim) {
+        outputradio = "COSD" + linefeed
+        radio.sendString(outputradio)
+        if (TILT == true) {
+            basic.showLeds(`
+                # # . # #
+                # . . . #
+                . . . . .
+                # . . . #
+                # # . # #
+                `)
+            if (UNTI == true) {
+                TILT = false
+                UNTI = false
             }
-            cos = (input.acceleration(Dimension.X) * refx + input.acceleration(Dimension.Y) * refy + input.acceleration(Dimension.Z) * refz) / (input.acceleration(Dimension.Strength) * refa)
-            if (cos < refcos - 0.8 * clim) {
-                outputradio = "COSE" + linefeed
-                radio.sendString(outputradio)
-                basic.showLeds(`
-                    # # # # #
-                    # . . . #
-                    # . . . #
-                    # . . . #
-                    # # # # #
-                    `)
-                TILT = true
-            } else if (cos > refcos - 0.8 * clim && cos < refcos - 0.6 * clim) {
-                outputradio = "COSD" + linefeed
-                radio.sendString(outputradio)
-                if (TILT == true) {
-                    basic.showLeds(`
-                        # # . # #
-                        # . . . #
-                        . . . . .
-                        # . . . #
-                        # # . # #
-                        `)
-                } else {
-                    basic.showLeds(`
-                        . # . # .
-                        # . . . #
-                        . . . . .
-                        # . . . #
-                        . # . # .
-                        `)
-                }
-            } else if (cos > refcos - 0.6 * clim && cos < refcos - 0.4 * clim) {
-                outputradio = "COSC" + linefeed
-                radio.sendString(outputradio)
-                if (TILT == true) {
-                    basic.showLeds(`
-                        # . . . #
-                        . # # # .
-                        . # . # .
-                        . # # # .
-                        # . . . #
-                        `)
-                } else {
-                    basic.showLeds(`
-                        . . . . .
-                        . # # # .
-                        . # . # .
-                        . # # # .
-                        . . . . .
-                        `)
-                }
-            } else if (cos > refcos - 0.4 * clim && cos < refcos - 0.2 * clim) {
-                outputradio = "COSB" + linefeed
-                radio.sendString(outputradio)
-                if (UNNE == true) {
-                    if (TILT != true) {
-                        outputradio = "ZIEH" + linefeed
-                        UNNE = false
-                        radio.sendString(outputradio)
-                        start = input.runningTime()
-                    } else {
-                        TILT = false
-                    }
-                }
-                if (TILT == true) {
-                    basic.showLeds(`
-                        # . . . #
-                        . . # . .
-                        . # . # .
-                        . . # . .
-                        # . . . #
-                        `)
-                } else {
-                    basic.showLeds(`
-                        . . . . .
-                        . . # . .
-                        . # . # .
-                        . . # . .
-                        . . . . .
-                        `)
-                }
-            } else {
-                outputradio = "COSA" + linefeed
-                radio.sendString(outputradio)
-                if (TILT == true) {
-                    basic.showLeds(`
-                        # . . . #
-                        . . . . .
-                        . . # . .
-                        . . . . .
-                        # . . . #
-                        `)
-                } else {
-                    basic.showLeds(`
-                        . . . . .
-                        . . . . .
-                        . . # . .
-                        . . . . .
-                        . . . . .
-                        `)
-                }
-                UNNE = true
-            }
+        } else {
+            basic.showLeds(`
+                . # . # .
+                # . . . #
+                . . . . .
+                # . . . #
+                . # . # .
+                `)
+        }
+    } else if (cos > refcos - 0.6 * clim && cos < refcos - 0.4 * clim) {
+        outputradio = "COSC" + linefeed
+        radio.sendString(outputradio)
+        if (TILT == true) {
+            basic.showLeds(`
+                # . . . #
+                . # # # .
+                . # . # .
+                . # # # .
+                # . . . #
+                `)
+        } else {
+            basic.showLeds(`
+                . . . . .
+                . # # # .
+                . # . # .
+                . # # # .
+                . . . . .
+                `)
+        }
+    } else if (cos > refcos - 0.4 * clim && cos < refcos - 0.2 * clim) {
+        outputradio = "COSB" + linefeed
+        radio.sendString(outputradio)
+        if (UNNE == true) {
             if (TILT != true) {
-                if (input.runningTime() - start >= 1500) {
-                    outputradio = "FALL" + linefeed
-                    radio.sendString(outputradio)
-                    start = input.runningTime()
-                }
+                outputradio = "ZIEH" + linefeed
+                UNNE = false
+                radio.sendString(outputradio)
+                start = input.runningTime()
             }
         }
+        if (TILT == true) {
+            basic.showLeds(`
+                # . . . #
+                . . # . .
+                . # . # .
+                . . # . .
+                # . . . #
+                `)
+        } else {
+            basic.showLeds(`
+                . . . . .
+                . . # . .
+                . # . # .
+                . . # . .
+                . . . . .
+                `)
+        }
     } else {
-        basic.showString("Calibrate!")
+        outputradio = "COSA" + linefeed
+        radio.sendString(outputradio)
+        if (TILT == true) {
+            basic.showLeds(`
+                # . . . #
+                . . . . .
+                . . # . .
+                . . . . .
+                # . . . #
+                `)
+        } else {
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                . . # . .
+                . . . . .
+                . . . . .
+                `)
+        }
+        UNNE = true
+        UNTI = true
+    }
+    if (input.runningTime() - start >= 1500) {
+        outputradio = "FALL" + linefeed
+        radio.sendString(outputradio)
     }
 })
